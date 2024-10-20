e34ee34242dc97bc17ee45189bf4c4b67ebbb652
Codeprince001 <urangyorkwisdom@gmail.com>
2024-10-20 23:39:26 +0100

    added pages

---
+++ client/src/pages/index.js
@@ -0,0 +1,9 @@
+import About from "./About";
+import Dashboard from "./Dashboard";
+import Home from "./Home";
+import Project from "./Project";
+import Signin from "./Signin";
+import Signup from "./Signup";
+
+
+export { About, Dashboard, Home, Project, Signin, Signup };
