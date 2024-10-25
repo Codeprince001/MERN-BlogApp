dbc41a3947a7df658df915c80c6cd2783f718b09
Codeprince001 <urangyorkwisdom@gmail.com>
2024-10-25 18:56:55 +0100

    validate user and handle error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -5,17 +5,25 @@ import { errorHandler } from "../utils/error.js";
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
 
+
   if (!username || !email || !password || username === "" || password === "" || email === "") {
     next(errorHandler(400, "all fields are required"));
   }
 
-  const hashedPassword = bcryptjs.hashSync(password, 10);
 
-  const newUser = new User({
-    username, email, password: hashedPassword
-  });
 
   try {
+    const existingUser = await User.findOne({ $or: [{ username }, { email }] });
+    if (existingUser) {
+      return next(errorHandler(409, "Username or email already exists"));
+    }
+
+    const hashedPassword = bcryptjs.hashSync(password, 10);
+
+    const newUser = new User({
+      username, email, password: hashedPassword
+    });
+
     await newUser.save();
     res.json({ message: "Signup successful" });
   } catch (error) {
