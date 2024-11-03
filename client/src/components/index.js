3976db8f89128f8482839d910a7cbfbed80c3443
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-04 00:14:45 +0100

    update component index

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -2,5 +2,7 @@ import Footer from "./Footer";
 import Header from "./Header";
 import Oauth from "./Oauth";
 import PrivateRoute from "./PrivateRoute";
+import Profile from "./Profile";
+import DashSidebar from "./DashSidebar";
 
-export { Header, Footer, Oauth, PrivateRoute };
+export { Header, Footer, Oauth, PrivateRoute, DashSidebar, Profile };
