bec6f7af28e600bc602cd29d4cd83045aaa424d5
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-03 14:44:46 +0100

    feat: set-up user account tab

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ import { useSelector } from "react-redux";
 
 const Header = () => {
   const path = useLocation().pathname;
-  const currentUser = useSelector(state => state.user);
+  const { currentUser } = useSelector(state => state.user);
 
   return (
     <Navbar className='border-b-2'>
@@ -32,8 +32,18 @@ const Header = () => {
           <FaMoon />
         </Button>
         {currentUser ? (
-          <Dropdown arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} />}>
-
+          <Dropdown className='p-1' arrowIcon={false} inline label={<Avatar alt='user' img={currentUser.profilePicture} rounded />}>
+            <Dropdown.Header>
+              <span className='block text-sm '>{currentUser.username}</span>
+              <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+            </Dropdown.Header>
+            <Dropdown.Header>
+              <Link to="/dashboard?tab=profile">
+                <Dropdown.Item>Profile</Dropdown.Item>
+              </Link>
+              <Dropdown.Divider />
+              <Dropdown.Item>Signout</Dropdown.Item>
+            </Dropdown.Header>
           </Dropdown>
         ) : (
           <Link to="/signin">
