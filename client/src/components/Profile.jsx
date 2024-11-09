a06b4d0628e65140b265ef9b3f9a0089dff76c4f
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-09 22:38:38 +0100

    Feat: s3 bucket image upload

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -9,12 +9,34 @@ const Profile = () => {
   const [imgFileUrl, setImgFileUrl] = useState(null);
   const filePickerRef = useRef();
 
-  useEffect(() => {
-    if (imgFile) {
-      uploadImage();
-    }
-  }, [imgFile]);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
+    const formData = new FormData();
+    formData.append("image", imgFile);
+    formData.append("username", e.target.username.value);
+    formData.append("email", e.target.email.value);
+
+
+    try {
+
+      const response = await fetch("api/user/upload-profile-picture", {
+        method: "POST",
+        body: formData,
+        headers: { 'Content-Type': 'multipart/form-data' }
+      });
+
+      const data = await response.json();
+      console.log(data);
+
+      if (!response.ok) {  // if status code is not 2xx, handle it as an error
+        dispatch(signInFailure(data.message || "Failed to upload Image"));
+        return;
+      }
+    } catch (error) {
+
+    }
+  };
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
@@ -24,13 +46,10 @@ const Profile = () => {
     }
   };
 
-
-
-
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="text-center font-semibold text-3xl my-7">Profile</h1>
-      <form className="flex flex-col gap-4">
+      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input hidden type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} />
         <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
           <img src={imgFileUrl || currentUser.profilePicture} alt="user" className="object-cover rounded-full w-full h-full border-8 border-[lightgray]" />
