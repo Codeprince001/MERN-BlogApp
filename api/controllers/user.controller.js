ab33b15fc2b8c0afd7e46b69f630e02741132e33
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-12 17:35:49 +0100

    update update proflie

--- api/controllers/user.controller.js
+++ api/controllers/user.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import * as dotenv from "dotenv";
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
-// import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
 dotenv.config();
 
@@ -19,46 +19,53 @@ const s3Client = new S3Client({
 });
 
 export const updateUserProfile = async (req, res) => {
-  const { userId, profilePictureUrl } = req.body;
+  const { userId } = req.user;
   const file = req.file;
-  const fileKey = `profile-pictures/${Date.now()}_${file.originalname.substring(0, 5).trim()}`;
+  let signedUrl;
 
-  if (!file) {
-    return res.status(400).json({ error: "No file provided" });
-  }
 
   try {
+    if (file) {
+      const fileKey = `profile-pictures/${Date.now()}_${file.originalname.substring(0, 5).trim()}`;
+      // Upload file to s3 Bucket
+      const uploadParams = {
+        Bucket: bucketName,
+        Key: fileKey,
+        Body: file.buffer,
+        ContentType: file.mimetype
+      };
 
-    // Upload file to s3 Bucket
-    const uploadParams = {
-      Bucket: bucketName,
-      Key: fileKey,
-      Body: file.buffer,
-      ContentType: file.mimetype
+      await s3Client.send(new PutObjectCommand(uploadParams));
+
+      // Generate a signed Url
+      const getObjectParams = {
+        Bucket: bucketName,
+        Key: fileKey,
+      };
+      signedUrl = await getSignedUrl(s3Client, new GetObjectCommand(getObjectParams), {
+        expiresIn: 3600,
+      });
+    }
+
+
+    const updateData = {
+      ...req.body,
+      ...(file && { profilePicture: signedUrl }),
     };
 
-    await s3Client.send(new PutObjectCommand(uploadParams));
+    delete updateData.userId;
+    delete updateData.email;
+    const updatedUser = await User.findByIdAndUpdate(userId, updateData, { new: true });
 
-    // Generate a signed Url
-    // const getObjectParams = {
-    //   Bucket: bucketName,
-    //   Key: fileKey,
-    // };
-    // const signedUrl = await getSignedUrl(s3Client, new GetObjectCommand(getObjectParams), {
-    //   expiresIn: 3600,
-    // });
+    if (!updatedUser) {
+      return res.status(404).json({ message: "User not found" });
+    }
 
-    // const updateUser = await User.findByIdAndUpdate(userId, { profilePicture: signedUrl }, { new: true });
-    // console.log(updateUser);
-
-    // if (!updateUser) {
-    //   return res.status(404).json({ message: "User not found" });
-    // }
-
-    // res.status(200).json({
-    //   message: "Profile picture updated successfully",
-    //   profilePicture: signedUrl,
-    // });
+    res.status(200).json({
+      message: "Profile updated successfully",
+      profilePicture: signedUrl,
+      user: updatedUser
+    });
   } catch (error) {
     console.error("Error uploading profile picture:", error);
     res.status(500).json({ message: "Image upload failed" });
