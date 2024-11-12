cacd8e64c0d6cfadf540e5a41a9f55b928d6076d
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-12 17:36:18 +0100

    user route setup

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { updateUserProfile } from "../controllers/user.controller.js";
 import { uploadSingle } from "../middlewares/multer.middleware.js";
+import { verifyToken } from "../utils/verifyUser.js";
 
 
 const router = express.Router();
 
 
 
-router.put("/update-profile", uploadSingle, updateUserProfile);
+router.put("/update-profile", verifyToken, uploadSingle, updateUserProfile);
 
 export default router;
