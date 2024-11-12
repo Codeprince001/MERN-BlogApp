d282cc3661ea7612fed692f10642343e4ca75232
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-12 17:37:44 +0100

    zod scheme for form validation

--- client/src/schemas/UserSchema.js
+++ client/src/schemas/UserSchema.js
@@ -1,6 +1,12 @@
 import { z } from "zod";
 
 export const profileSchema = z.object({
-  username: z.string().min(1, "Username cannot be empty"),
+  username: z.string({
+    required_error: "Username cannot be blank"
+  }).min(5, { message: "must be a mininum of 5 character" }),
   email: z.string().email("Enter a valid email address"),
+  password: z.string().min(8, "Password must be at least 8 characters long.\n")
+    .regex(/[A-Z]/, "\n Password must contain at least one uppercase letter.\n")
+    .regex(/[a-z]/, "\n Password must contain at least one lowercase letter.\n")
+    .regex(/[0-9]/, "\n Password must contain at least one number.\n")
 });
