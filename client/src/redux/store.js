ffedddcbd3572b5c0cdac2d984cf0b40ad5dc0b9
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-13 23:18:13 +0100

    feat: allow state expire, with cookies

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -4,16 +4,21 @@ import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import persistStore from 'redux-persist/es/persistStore';
 import themeReducer from "./features/theme/themeSlice";
+import expireInTransfrom from "redux-persist-transform-expire-in";
 
 const rootReducer = combineReducers({
   user: userReducer,
   theme: themeReducer
 });
 
+const expiresIn = 24 * 60 * 60 * 1000;
+const expirationKey = import.meta.env.EXPIRATION_KEY;
+
 const persistConfig = {
   key: "root",
   storage,
-  version: 1
+  version: 1,
+  transforms: [expireInTransfrom(expiresIn, expirationKey, [])],
 };
 
 const persistedReducer = persistReducer(persistConfig, rootReducer);
