7d345ae64c9641f1575ba8bd3b3d99a1bbdc398e
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-15 18:01:24 +0100

    Setup api route to delete user

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { updateUserProfile } from "../controllers/user.controller.js";
+import { deleteUser, updateUserProfile } from "../controllers/user.controller.js";
 import { uploadSingle } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
@@ -8,6 +8,9 @@ const router = express.Router();
 
 
 
-router.put("/update-profile", verifyToken, uploadSingle, updateUserProfile);
+router.put("/update-profile/:userId", verifyToken, uploadSingle, updateUserProfile);
+router.delete("/delete/:userId", verifyToken, deleteUser);
+
+
 
 export default router;
