c548660e2a96c9410b205ce2af33dff0520a404a
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-15 19:30:14 +0100

    add signout functionality

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -2,13 +2,16 @@ import { Sidebar } from "flowbite-react";
 import { useEffect, useState } from "react";
 import { FaUser } from "react-icons/fa";
 import { IoLogOutOutline } from "react-icons/io5";
+import { useDispatch } from "react-redux";
 import { Link, useLocation, useNavigate } from "react-router-dom";
+import { signoutSuccess } from "../redux/features/users/userSlice";
 
 
 const DashSidebar = () => {
   const location = useLocation();
   const [tab, setTab] = useState("");
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
@@ -19,6 +22,25 @@ const DashSidebar = () => {
   }, [location.search]);
 
 
+  const handleSignout = async () => {
+    try {
+      const res = await fetch("/api/user/signout/", {
+        method: "POST"
+      });
+
+      const data = res.json();
+      if (!res.ok) {
+        console.log(data.message);
+      } else {
+        dispatch(signoutSuccess());
+      }
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+
+
+
 
   return (
     <Sidebar className="w-full md:w-56">
@@ -27,7 +49,7 @@ const DashSidebar = () => {
           <Sidebar.Item onClick={() => navigate("/dashboard?tab=profile")} active={tab === "profile"} icon={FaUser} label={"User"} labelColor="dark">
             Profile
           </Sidebar.Item>
-          <Sidebar.Item icon={IoLogOutOutline} className="cursor-pointer">
+          <Sidebar.Item icon={IoLogOutOutline} className="cursor-pointer" onClick={handleSignout}>
             Signout
           </Sidebar.Item>
         </Sidebar.ItemGroup>
