b342cba98edb0fe15e6ab2d3c44f8ec9708a3209
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-15 19:01:31 +0100

    feat: setup user functionality

--- client/src/components/Profile.jsx
+++ client/src/components/Profile.jsx
@@ -1,27 +1,31 @@
-import { Alert, Button, TextInput } from "flowbite-react";
+import { Alert, Button, Modal, TextInput } from "flowbite-react";
 import { useEffect, useRef, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { profileSchema } from "../schemas/UserSchema";
-import { signInSuccess, updateStart, updateFailure, updateSuccess } from "../redux/features/users/userSlice";
+import { signInSuccess, updateStart, updateFailure, updateSuccess, deleteUserFailure, deleteUserStart, deleteUserSuccess } from "../redux/features/users/userSlice";
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
+
+import { HiOutlineExclamationCircle } from "react-icons/hi";
 
 
 const Profile = () => {
   const dispatch = useDispatch();
-  const { currentUser } = useSelector((state) => state.user);
+  const { currentUser, error } = useSelector((state) => state.user);
+
+
   const [imgFile, setImgFile] = useState(null);
   const [imgFileUrl, setImgFileUrl] = useState(currentUser.profilePicture);
   const [errors, setErrors] = useState({});
-  const [updateUserSucces, setUpdateUserSuccess] = useState(null);
-  const [user, setUser] = useState(currentUser || {
-    username: "",
-    email: "",
-    profilePicture: ""
-  });
+  const [updateUserSuccess, setUpdateUserSuccess] = useState(null);
+  const [updateUserError, setUpdateUserError] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+
   const filePickerRef = useRef();
 
   const handleSubmit = async (e) => {
+    setUpdateUserSuccess();
+    setUpdateUserError(null);
     e.preventDefault();
 
 
@@ -57,25 +61,36 @@ const Profile = () => {
       formData.append("image", imgFile);
     }
 
+    // Check if there are actual changes
+    const isUsernameUnchanged = Data.username === currentUser.username;
+    const isEmailUnchanged = Data.email === currentUser.email;
+    const isPasswordEmpty = !Data.password;
+    const isImageUnchanged = !imgFile;
+
+    if (isUsernameUnchanged && isEmailUnchanged && isPasswordEmpty && isImageUnchanged) {
+      setUpdateUserError("No changes to update");
+      return;
+    }
 
     try {
-      // dispatch(updateStart());
-      const response = await fetch("api/user/update-profile", {
+      dispatch(updateStart());
+      const response = await fetch(`api/user/update-profile/${currentUser._id}`, {
         method: "PUT",
         body: formData,
       });
 
       const data = await response.json();
 
-      if (!response.ok) {  // if status code is not 2xx, handle it as an error
-        dispatch(signInFailure(data.message || "Failed to upload Image"));
+      if (!response.ok) {  // if status code is not 200, handle it as an error
+        dispatch(updateFailureFailure(data.message || "Failed to upload Image"));
         return;
       } else {
         dispatch(updateSuccess(data.user));
         setUpdateUserSuccess("User's Profile Updated Successfully");
       }
     } catch (error) {
-
+      dispatch(updateFailure(error.message));
+      setUpdateUserError(error.message);
     }
   };
 
@@ -88,9 +103,32 @@ const Profile = () => {
     }
   };
 
+  const handleDeleteUser = async () => {
+    setShowModal(false);
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE"
+      });
+
+      const data = await res.json();
+      if (!res.ok) {
+        dispatch(deleteUserFailure(data.message));
+      } else {
+        dispatch(deleteUserSuccess());
+      }
+    } catch (error) {
+      dispatch(deleteUserFailure());
+    }
+  };
+
   return (
     <div className="max-w-lg mx-auto p-3 w-full">
       <h1 className="text-center font-semibold text-3xl my-7">Profile</h1>
+
+
+      {/* Form */}
+
       <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
         <input hidden type="file" accept="image/*" onChange={handleImageChange} ref={filePickerRef} />
         <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={() => filePickerRef.current.click()}>
@@ -108,11 +146,30 @@ const Profile = () => {
           Update
         </Button>
       </form>
+
+
       <div className="text-red-500 flex justify-between mt-5">
-        <span className="cursor-pointer">Delete Account</span>
-        <span className="cursor-pointer">Sign Out</span>
+        <span className="cursor-pointer" onClick={() => setShowModal(true)}>Delete Account</span>
+        <span className="cursor-pointer" onClick={handleSignout}>Sign Out</span>
       </div>
-      {updateUserSucces && <Alert color="success" className="mt-5">{updateSuccess}</Alert>}
+      {updateUserSuccess && <Alert color="success" className="mt-5">{updateUserSuccess}</Alert>}
+      {error && <Alert color="failure" className="mt-5">{error}</Alert>}
+
+      <Modal show={showModal} onClose={() => setShowModal(false)} popup size="md">
+        <Modal.Header />
+        <Modal.Body>
+          <div className="text-center">
+            <HiOutlineExclamationCircle className="text-5xl text-gray-200 dark:text-gray-200 mb-4 mx-auto" />
+            <h3 className="mb-5 text-lg text-gray-500 dark:text-gray-400">Are you sure you want to delete your account</h3>
+            <div className="flex justify-center gap-4">
+              <Button color="failure" onClick={handleDeleteUser}>Yes I'm Sure</Button>
+              <Button color="gray" onClick={() => setShowModal(false)}>No, Cancel</Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
+
+
     </div >
   );
 };
