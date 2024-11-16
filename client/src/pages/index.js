412f206b01f8848f4b6bf117c6e36f9f2aa359f4
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-16 20:09:39 +0100

    page export

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Project from "./Project";
 import Signin from "./Signin";
 import Signup from "./Signup";
+import CreatePost from "./CreatePost";
 
 
-export { About, Dashboard, Home, Project, Signin, Signup };
+export { About, Dashboard, Home, Project, Signin, Signup, CreatePost };
