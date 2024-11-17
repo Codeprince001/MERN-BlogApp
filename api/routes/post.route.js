f7317cc22a47ffbd56a0a9675e8103a8252c31fc
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-17 23:38:21 +0100

    add imgUpload route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,11 @@
-import { create } from "../controllers/post.controller.js";
+import { create, postImageUpload } from "../controllers/post.controller.js";
+import { uploadSingle } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import express from "express";
 
 const router = express.Router();
 
 router.post("/create", verifyToken, create);
+router.post("/upload-postImage", uploadSingle, postImageUpload);
 
 export default router;
