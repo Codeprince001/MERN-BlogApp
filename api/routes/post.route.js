2ae5d9a265a882dd102e1678bdbe10ccdc7da3b4
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-22 17:24:51 +0100

    add getpost route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,4 +1,4 @@
-import { create, postImageUpload } from "../controllers/post.controller.js";
+import { create, postImageUpload, getPosts } from "../controllers/post.controller.js";
 import { uploadSingle } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import express from "express";
@@ -7,5 +7,6 @@ const router = express.Router();
 
 router.post("/create", verifyToken, create);
 router.post("/upload-postImage", uploadSingle, postImageUpload);
+router.get("getPosts", getPosts);
 
 export default router;
