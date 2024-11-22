cbcc0124b82a6f412038690b9c008bbaac5b8adb
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-22 17:24:05 +0100

    feat: create getPost endpoin

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -79,3 +79,37 @@ export const postImageUpload = async (req, res, next) => {
     res.status(500).json({ message: "Image upload failed" });
   }
 };
+
+export const getPosts = async (req, res, next) => {
+  try {
+    const startIndex = parseInt(req.query.startIndex) || 0;
+    const limit = parseInt(req.query.limit) || 9;
+    const sortDirection = req.query.order === "asc" ? 1 : -1;
+    const posts = await Post.find(
+      ...(req.query.userId && { userId: req.query.userId }),
+      ...(req.query.category && { category: req.query.category }),
+      ...(req.query.slug && { slug: req.query.slug }),
+      ...(req.query.postId && { _id: req.query.postId }),
+      ...(req.query.searchTerm && {
+        $or: [
+          { title: { $regex: req.query.searchTerm, $options: "i" } },
+          { content: { $regex: req.query.searchTerm, $options: "i" } },
+        ]
+      })
+        .sort({ updatedAt: sortDirection }).skip(startIndex).limit(limit)
+    );
+
+    const totalPosts = await Post.countDocuments();
+    const timeNow = new Date();
+    const oneMonthAgo = new Date(
+      timeNow.getFullYear(),
+      timeNow.getMonth() - 1,
+      timeNow.getDate()
+    );
+    const lastMonthsPosts = await Post.countDocuments({ createdAt: { $gte: oneMonthAgo } });
+
+    res.status(200).json({ posts, totalPosts, lastMonthsPosts });
+  } catch (error) {
+    next(error);
+  }
+};
