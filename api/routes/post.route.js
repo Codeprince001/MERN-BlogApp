479560b2c57565915cf59300a8f6f721a8ec6142
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-25 23:13:36 +0100

    delete -post route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,4 +1,4 @@
-import { create, postImageUpload, getPosts } from "../controllers/post.controller.js";
+import { create, postImageUpload, getPosts, deletePost } from "../controllers/post.controller.js";
 import { uploadSingle } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import express from "express";
@@ -8,5 +8,6 @@ const router = express.Router();
 router.post("/create", verifyToken, create);
 router.post("/upload-postImage", uploadSingle, postImageUpload);
 router.get("/getPosts", getPosts);
+router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
 
 export default router;
