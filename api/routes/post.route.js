bd26669142fb20f7b037a6cccc78e8cd69ab7602
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-26 13:53:36 +0100

    add update post route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,4 +1,4 @@
-import { create, postImageUpload, getPosts, deletePost } from "../controllers/post.controller.js";
+import { create, postImageUpload, getPosts, deletePost, updatePost } from "../controllers/post.controller.js";
 import { uploadSingle } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../utils/verifyUser.js";
 import express from "express";
@@ -9,5 +9,6 @@ router.post("/create", verifyToken, create);
 router.post("/upload-postImage", uploadSingle, postImageUpload);
 router.get("/getPosts", getPosts);
 router.delete("/deletepost/:postId/:userId", verifyToken, deletePost);
+router.put("/updatepost/:postId/:userId", verifyToken, updatePost);
 
 export default router;
