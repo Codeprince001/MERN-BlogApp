f4cc70a036ef31c1de9a7fb0d4b37b83b235377a
Codeprince001 <urangyorkwisdom@gmail.com>
2024-11-29 03:14:46 +0100

    add getUser api route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,16 +1,14 @@
 import express from "express";
-import { deleteUser, signout, updateUserProfile } from "../controllers/user.controller.js";
+import { deleteUser, getUsers, signout, updateUserProfile } from "../controllers/user.controller.js";
 import { uploadSingle } from "../middlewares/multer.middleware.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
-
 const router = express.Router();
-
-
 
 router.put("/update-profile/:userId", verifyToken, uploadSingle, updateUserProfile);
 router.delete("/delete/:userId", verifyToken, deleteUser);
 router.post("/signout", signout);
+router.get("/getusers", verifyToken, getUsers);
 
 
 export default router;
