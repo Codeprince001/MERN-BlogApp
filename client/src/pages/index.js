e89238eefc942b1b3a99a88aeb6e2f3a6642c785
Codeprince001 <urangyorkwisdom@gmail.com>
2024-12-01 16:04:55 +0100

    export component

--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -6,6 +6,6 @@ import Signin from "./Signin";
 import Signup from "./Signup";
 import CreatePost from "./CreatePost";
 import UpdatePost from "./UpdatePost";
+import Post from "./Post";
 
-
-export { About, Dashboard, Home, Project, Signin, Signup, CreatePost, UpdatePost };
+export { Post, About, Dashboard, Home, Project, Signin, Signup, CreatePost, UpdatePost };
