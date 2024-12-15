da343372bd7cd1529b7da11e650a04838f3a5546
Codeprince001 <urangyorkwisdom@gmail.com>
2024-12-15 23:22:57 +0100

    add like functionality

--- client/src/components/Comments.jsx
+++ client/src/components/Comments.jsx
@@ -1,8 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import moment from "moment";
+import { FaThumbsUp } from "react-icons/fa6";
+import { useSelector } from 'react-redux';
 
-const Comments = ({ comment }) => {
+
+const Comments = ({ comment, onLike }) => {
   const [user, setUser] = useState({});
+  const { currentUser } = useSelector(state => state.user);
+
 
   useEffect(() => {
     const getUser = async () => {
@@ -34,6 +39,13 @@ const Comments = ({ comment }) => {
           <span className='text-xs text-gray-500'>{moment(comment.createdAt).fromNow()}</span>
         </div>
         <p className='text-gray-500 mb-2'>{comment.content}</p>
+        <div className='flex items-center pt-2 text-xs border-t dark:border-gray-700 max-w-fit gap-2'>
+          <button onClick={() => onLike(comment._id)} className={`text-gray-400 hover:text-blue-400 ${currentUser && comment.likes.includes(currentUser._id) && "!text-blue-500"
+            }`}><FaThumbsUp className='text-sm' /></button>
+          <p className='text-gray-400'>{
+            comment.numberOfLikes > 0 && comment.numberOfLikes + " " + (comment.numberOfLikes === 1 ? "like" : "likes")
+          }</p>
+        </div>
       </div>
 
     </div>
