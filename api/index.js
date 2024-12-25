a19c944be5aa7e7eb79797ed4418ec3a25956587
Codeprince001 <urangyorkwisdom@gmail.com>
2024-12-25 03:28:59 +0100

    Configuration for deployment

--- api/index.js
+++ api/index.js
@@ -7,6 +7,7 @@ import { errorMiddlewareHandler } from "./middlewares/error.middleware.js";
 import CookieParser from "cookie-parser";
 import postRoutes from "./routes/post.route.js";
 import commentRoutes from "./routes/comment.route.js";
+import path from "path";
 
 dotenv.config();
 
@@ -23,10 +24,18 @@ mongoose.connect(process.env.MONGODb_URI)
     console.log(err);
   });
 
+const __dirname = path.resolve();
+
 app.use("/api/user", userRoute);
 app.use("/api/auth", authRoute);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
 
 app.use(errorMiddlewareHandler);
 
