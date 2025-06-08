2182f516a28c33dab633d9a5892944b000c443c0
Wisdom Matthew <109921600+Codeprince001@users.noreply.github.com>
2025-06-08 13:43:48 +0100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -31,12 +31,6 @@ app.use("/api/auth", authRoute);
 app.use("/api/post", postRoutes);
 app.use("/api/comment", commentRoutes);
 
-app.use(express.static(path.join(__dirname, "/client/dist")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
-});
-
 app.use(errorMiddlewareHandler);
 
 app.use((req, res) => {
